16aeb461f513bf820e09d2b51b50f6c313d0f3bb
tunapro1234 <tunagul54@gmail.com>
2023-03-29 01:58:16 +0300

    parse

--- main.c
+++ main.c
@@ -13,32 +13,79 @@ typedef struct int_node_t {
 
 
 typedef struct node_node_t {
+	/* next ile başlamıyor çünkü int_node_t alan */ 
+	/* fonksiyonlarda normalde nexte yazdırılacak */ 
+	/* şeylerin buraya yazdırılmasını istiyorum */
 	int_node_t *row_start;
 	struct node_node_t *next;
 } node_node_t;
 
+typedef struct options_t {
+	bool print_matrix, count_zeros, find_duplicates;	
+	char* input_file;
+	char* output_file;
+} options_t;
 
-node_node_t* read_csv(char* filename);
 node_node_t* new_node_node(node_node_t* self);
 int_node_t* new_int_node(int_node_t* self);
+node_node_t* read_csv(char* filename);
 void print_node(node_node_t* self);
 
-int main() {
-    /* char* input_file = "input.csv"; */
-    /* char* output_file = "output.txt"; */
-    /* char* option = "--print"; */
-    /* for (int i = 1; i < argc; i++) { */
-    /*     if (argv[i][0] == '-') { */
-    /*         option = argv[i]; */
-    /*         if (i + 1 < argc && argv[i+1][0] != '-') { */
-    /*             if (argv[i][1] == 'i') input_file = argv[i+1]; */
-    /*             else if (argv[i][1] == 'o') output_file = argv[i+1]; */
-    /*         } */
-    /*     } */
-    /* } */
+/*
+ * TODO
+ * free nodes
+ *
+ */
+
+options_t* options_new() {
+	options_t *self = (options_t *)malloc(sizeof(options_t));
+
+	self->print_matrix = 0;
+	self->count_zeros = 0;
+	self->find_duplicates = 0;	
+	self->input_file = NULL;
+	self->output_file = NULL;
+
+	return self;
+}
+
+options_t* parse_inputs(int argc, char** argv) {
+	options_t *options = options_new();
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
+            options->input_file = argv[i+1];
+            i++;
+        }
+        else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
+            options->output_file = argv[i+1];
+            i++;
+        }
+        else if (strcmp(argv[i], "--print") == 0) {
+            options->print_matrix = 1;
+        }
+        else if (strcmp(argv[i], "--nzeros") == 0) {
+            options->count_zeros = 1;
+        }
+        else if (strcmp(argv[i], "--duplicates") == 0) {
+            options->find_duplicates = 1;
+        }
+        else {
+            fprintf(stderr, "Error: unknown option '%s'\n", argv[i]);
+            exit(1);
+        }
+    }
+	return options;
+}
+
+int main(int argc, char** argv) {
+	options_t* options = parse_inputs(argc, argv);
+	printf("print_matrix: %d\n", options->print_matrix);
+	printf("count_zeros: %d\n", options->count_zeros);
+	printf("find_duplicates: %d\n", options->find_duplicates);
 	
-	node_node_t *data = read_csv(FILENAME);
-	print_node(data);
+	/* node_node_t *data = read_csv(FILENAME); */
+	/* print_node(data); */
 	return 0;
 }
 
